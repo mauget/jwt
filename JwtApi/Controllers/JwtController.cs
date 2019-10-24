27abab00a9f063d299b1be67d927ce23f4177288
mauget <mauget@gmail.com>
2019-10-24 16:02:11 -0400

    Set the comments path for the Swagger JSON and UI

--- JwtApi/Controllers/JwtController.cs
+++ JwtApi/Controllers/JwtController.cs
@@ -19,20 +19,34 @@ namespace JwtApi.Controllers
         }
         
         /// <summary>
-        /// Creates a base64-encoded JSON Web Token having a payload extracted from the passed model.'
+        /// Creates a signed base64-url-encoded JSON Web Token having a payload extracted from the passed JwtPayload.
         /// </summary>
-        /// <param name="payload"></param>
-        /// <returns>base64-encoded JWT string</returns>
+        /// <remarks>
+        /// Sample request:
+        /// 
+        ///     POST /api/Jwt
+        ///     {
+        ///        "sub": "JWT Demo",
+        ///        "iss": "lem",
+        ///        "aud": "Developers",
+        ///        "exp": "1571942604066",
+        ///        "manager": "false",
+        ///        "admin": "true"
+        ///     }
+        ///
+        /// </remarks>
+        /// <param name="jwtPayload"></param>
+        /// <returns>base64-url-encoded JWT string</returns>
         [HttpPost]
-        public ActionResult<string> Encode([FromBody] JwtPayload payload)
+        public ActionResult<string> Encode([FromBody] JwtPayload jwtPayload)
         {
-            return Ok(_jwtService.Encode(payload));
+            return Ok(_jwtService.Encode(jwtPayload));
         }
 
         /// <summary>
-        /// Decodes a model from the payload of the given base64-encoded JSON Web Token
+        /// Decodes a model from the payload of a signed base64-url-encoded JSON Web Token
         /// </summary>
-        /// <param name="token">A base64-encoded JWT</param>
+        /// <param name="token">A base64-url-encoded JWT</param>
         /// <returns>JwtPayload</returns>
         [HttpGet]
         public ActionResult<JwtPayload> Decode(string token)
